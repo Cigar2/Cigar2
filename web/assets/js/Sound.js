a5488b0e6ff25a19312f0f8c9afb88a4e4acb5cc
Tom Burris <unsaneity@gmail.com>
2018-04-24 09:59:17 -0700

    changed class to ES5 compatible

--- web/assets/js/Sound.js
+++ web/assets/js/Sound.js
@@ -1,29 +1,27 @@
-class Sound {
-    constructor(src, volume = 0.5, maximum = Infinity) {
-        this.src = src;
-        this.volume = volume;
-        this.maximum = maximum;
-        this.elms = [];
-    }
-    play(vol = this.volume) {
-        this.volume = vol;
-        let toPlay;
-        for (const elm of this.elms) {
-            if (elm.paused) {
-                toPlay = elm;
-                break;
-            }
-        }
-        if (!toPlay) toPlay = this.add();
-        toPlay.volume = this.volume;
-        toPlay.play();
-    }
-    add() {
-        if (this.elms.length >= this.maximum) {
-            return this.elms[0];
-        }
-        const elm = new Audio(this.src);
-        this.elms.push(elm);
-        return elm;
-    }
+function Sound(src, volume = 0.5, maximum = Infinity) {
+    this.src = src;
+    this.volume = volume;
+    this.maximum = maximum;
+    this.elms = [];
 }
+Sound.prototype.play = function(vol = this.volume) {
+    this.volume = vol;
+    let toPlay;
+    for (const elm of this.elms) {
+        if (elm.paused) {
+            toPlay = elm;
+            break;
+        }
+    }
+    if (!toPlay) toPlay = this.add();
+    toPlay.volume = this.volume;
+    toPlay.play();
+};
+Sound.prototype.add = function() {
+    if (this.elms.length >= this.maximum) {
+        return this.elms[0];
+    }
+    const elm = new Audio(this.src);
+    this.elms.push(elm);
+    return elm;
+};

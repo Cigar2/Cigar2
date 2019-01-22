737b9120e0108b076b812107531fc63892565389
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2019-01-22 20:14:34 +0100

    Removed callback, added non-png warning

--- updateSkinList.js
+++ updateSkinList.js
@@ -1,7 +1,11 @@
 const fs = require("fs"),
       folderContents = fs.readdirSync("./web/skins"),
-      fileNames = folderContents.filter(f => f.endsWith(".png"));
-fs.writeFileSync("./web/skinList.txt", fileNames.map(f => f.slice(0, -4)).join(), err => {
-    if (err) throw err;
-});
+      pngFileNames = folderContents.filter(f => f.endsWith(".png"));
+
+if(folderContents.length !== pngFileNames.length) {
+  const nonPngFileNames = folderContents.filter(f => !f.endsWith(".png"));
+  console.warn("Warning: found " + nonPngFileNames.length + " non-PNG skins, these will not be loaded! (" + nonPngFileNames.join(", ") + ")");
+}
+
+fs.writeFileSync("./web/skinList.txt", pngFileNames.map(f => f.slice(0, -4)).join());
 console.log("Successfully updated skinList.txt!");

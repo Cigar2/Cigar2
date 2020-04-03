ef44f8e2e3e2a2f2557bafa5171ee8fcc6b09dfc
jwpsr <jwparkersr@gmail.com>
2020-04-02 19:11:09 -0500

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,1 +1,1 @@
-const express=require('express'),app=express();app.use(express.static('web'));app.listen(8080);
+const express=require('express'),app=express();app.use(express.static('web'));app.listen(process.env.PORT);
